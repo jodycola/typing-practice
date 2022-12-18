c57e288110affe78aa114f31d98db89b1551c6f9
Kody Samaroo <kody.samaroo@gmail.com>
2022-12-18 18:51:52 -0500

    setting up react components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Text from './Text';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Text/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Text.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Text() {
+
+    const charPool = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+    return (
+        <div>Can hold state</div>
+    )
+}
